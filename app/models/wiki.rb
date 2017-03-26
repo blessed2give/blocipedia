a2095acdd6749eafdd11c927ce8652baadc8ce37
Paul <paucho1437@yahoo.com>
2017-03-26 17:01:15 -0500

    private wikis completed

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,4 +1,4 @@
 class Wiki < ActiveRecord::Base
   belongs_to :user
-  scope :visible_to, -> (user) { user && user.role != 0 ? all : joins(:wiki).where('wikis.private' => false )}
+  scope :visible_to, -> (user) { user && user.role != 'standard' ? all : where(private: false )}
 end
